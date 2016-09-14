2d302c5a4519f7ec93b7b6072748c716eba02842
Christoph Junghans <junghans@lanl.gov>
2016-09-14 11:02:05 -0600

    Update legion build

--- cmake/gasnet.cmake
+++ cmake/gasnet.cmake
@@ -6,5 +6,5 @@ set(GASNET_MD5 "ff204fe3df02a92d485371240999d395")
 ExternalProject_Add(${GASNET_NAME}
  URL ${GASNET_URL}/${GASNET_GZ}
  URL_MD5 ${GASNET_MD5}
- CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${CMAKE_INSTALL_PREFIX} --enable-par --disable-aligned-segments  --enable-mpi-compat --enable-segment-fast --disable-aligned-segments --disable-pshm --with-segment-mmap-max=4GB CFLAGS=-g
+ CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${CMAKE_INSTALL_PREFIX} --enable-par --disable-aligned-segments  --enable-mpi-compat --enable-segment-fast --disable-aligned-segments --disable-pshm --with-segment-mmap-max=4GB CFLAGS=-g "CC=${CMAKE_C_COMPILER} -fPIC" "CXX=${CMAKE_CXX_COMPILER} -fPIC" "MPI_CC=mpicc -fPIC"
 )
