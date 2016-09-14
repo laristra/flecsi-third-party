b34534c71a315ae483f6f3b82b38a25f8617a4de
Galen M Shipman <gshipman@lanl.gov>
2016-09-14 17:51:25 -0400

    legion and gasnet build fixes

--- cmake/gasnet.cmake
+++ cmake/gasnet.cmake
@@ -3,30 +3,31 @@ set(GASNET_URL ${PROJECT_SOURCE_DIR}/files)
 set(GASNET_GZ  GASNet-1.26.4.tar)
 set(GASNET_MD5 "ff204fe3df02a92d485371240999d395")
 
-if(GASNET_CODNUIT STREQUAL "udp")
+if(GASNet_CONDUIT STREQUAL "udp")
 
 ExternalProject_Add(${GASNET_NAME}
  URL ${GASNET_URL}/${GASNET_GZ}
  URL_MD5 ${GASNET_MD5}
- CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${CMAKE_INSTALL_PREFIX}  --disable-ibv --enable-udp --enable-par --disable-aligned-segments  --enable-mpi-compat --enable-segment-fast --disable-aligned-segments --disable-pshm --with-segment-mmap-max=4GB CFLAGS=-g --disable-mpi 
+ CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${CMAKE_INSTALL_PREFIX}  --disable-ibv --enable-udp --enable-par --disable-aligned-segments  --enable-mpi-compat --enable-segment-fast --disable-aligned-segments --disable-pshm --with-segment-mmap-max=4GB --disable-mpi  "CC=${CMAKE_C_COMPILER} -fPIC" "CXX=${CMAKE_CXX_COMPILER} -fPIC" "MPI_CC=mpicc -fPIC"
 )
 
-elseif (GASNET_CODNUIT STREQUAL "ibv")
+elseif (GASNet_CONDUIT STREQUAL "ibv")
 
 ExternalProject_Add(${GASNET_NAME}
  URL ${GASNET_URL}/${GASNET_GZ}
  URL_MD5 ${GASNET_MD5}
- CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${CMAKE_INSTALL_PREFIX}  --disable-udp --enable-ibv --enable-par --disable-aligned-segments  --enable-mpi-compat --enable-segment-fast --disable-aligned-segments --disable-pshm --with-segment-mmap-max=4GB CFLAGS=-g --disable-mpi
+ CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${CMAKE_INSTALL_PREFIX}  --disable-udp --enable-ibv --enable-par --disable-aligned-segments  --enable-mpi-compat --enable-segment-fast --disable-aligned-segments --disable-pshm --with-segment-mmap-max=4GB --disable-mpi "CC=${CMAKE_C_COMPILER} -fPIC" "CXX=${CMAKE_CXX_COMPILER} -fPIC" "MPI_CC=mpicc -fPIC"
 )
 
-elseif (GASNET_CODNUIT STREQUAL "mpi")
+elseif (GASNet_CONDUIT STREQUAL "mpi")
 
 ExternalProject_Add(${GASNET_NAME}
  URL ${GASNET_URL}/${GASNET_GZ}
  URL_MD5 ${GASNET_MD5}
- CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${CMAKE_INSTALL_PREFIX}  --enable-mpi --enable-par --disable-aligned-segments  --enable-mpi-compat --enable-segment-fast --disable-aligned-segments --disable-pshm --with-segment-mmap-max=4GB CFLAGS=-g 
+ 
+ CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${CMAKE_INSTALL_PREFIX}  --enable-mpi --disable-udp --enable-par --disable-aligned-segments  --enable-mpi-compat --enable-segment-fast --disable-aligned-segments --disable-pshm --with-segment-mmap-max=4GB "CC=${CMAKE_C_COMPILER} -fPIC" "CXX=${CMAKE_CXX_COMPILER} -fPIC" "MPI_CC=mpicc -fPIC"  
 )
-
+  
 else() 
   message (ERROR "wrong Gasnet conduit specified")
 endif ()
