3d2a9652eca0f33c5924784d872093dafcd1afd7
Christoph Junghans <junghans@lanl.gov>
2016-09-14 16:31:45 -0600

    ported some of Galen's hacks

--- cmake/gasnet.cmake
+++ cmake/gasnet.cmake
@@ -3,8 +3,16 @@ set(GASNET_URL ${PROJECT_SOURCE_DIR}/files)
 set(GASNET_GZ  GASNet-1.26.4.tar)
 set(GASNET_MD5 "ff204fe3df02a92d485371240999d395")
 
+if(GASNet_CONDUIT STREQUAL "udp")
+   set(CONF_OPTS --enable-udp --disable-mpi)
+elseif(GASNet_CONDUIT STREQUAL "mpi")
+   set(CONF_OPTS --enable-mpi --disable-udp)
+else() 
+  message (ERROR "wrong Gasnet conduit specified")
+endif ()
+ 
 ExternalProject_Add(${GASNET_NAME}
  URL ${GASNET_URL}/${GASNET_GZ}
  URL_MD5 ${GASNET_MD5}
- CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${CMAKE_INSTALL_PREFIX} --enable-par --disable-aligned-segments  --enable-mpi-compat --enable-segment-fast --disable-aligned-segments --disable-pshm --with-segment-mmap-max=4GB CFLAGS=-g "CC=${CMAKE_C_COMPILER} -fPIC" "CXX=${CMAKE_CXX_COMPILER} -fPIC" "MPI_CC=mpicc -fPIC"
+ CONFIGURE_COMMAND <SOURCE_DIR>/configure --prefix=${CMAKE_INSTALL_PREFIX}  ${CONF_OPTS} --disable-ibv --enable-par --disable-aligned-segments  --enable-mpi-compat --enable-segment-fast --disable-aligned-segments --disable-pshm --with-segment-mmap-max=4GB "CC=${CMAKE_C_COMPILER} -fPIC" "CXX=${CMAKE_CXX_COMPILER} -fPIC" "MPI_CC=mpicc -fPIC"
 )
